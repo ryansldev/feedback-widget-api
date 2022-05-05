73909bfe6909dc4849184aa963276ee67b9992d6
ryansldev <ryanlima1309@gmail.com>
2022-05-05 20:32:34 -0300

    feat: creating feedback

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,20 @@
 import express, { json } from "express";
+import { prisma } from "./prisma";
 
 const app = express();
 
 app.use(json());
 
-app.post("/feedbacks", (request, response) => {
-  console.log(request.body);
-  return response.send("Hello World")
+app.post("/feedbacks", async (request, response) => {
+  const { type, comment, screenshot } = request.body;
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  })
+  return response.status(201).json({ data: feedback });
 })
 
 app.listen(3333, () => {
