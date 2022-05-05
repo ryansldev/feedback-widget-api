f6790709d991192768c348042e10559bc827d2ac
ryansldev <ryanlima1309@gmail.com>
2022-05-05 20:58:52 -0300

    feat: sending mail

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,24 @@
+import "dotenv/config";
 import express, { json } from "express";
+import nodemailer from "nodemailer";
 import { prisma } from "./prisma";
+
+console.log(process.env);
 
 const app = express();
 
 app.use(json());
+
+const { MAIL_HOST, MAIL_PORT, MAIL_USER, MAIL_PASS } = process.env;
+
+const transport = nodemailer.createTransport({
+  host: MAIL_HOST,
+  port: Number(MAIL_PORT),
+  auth: {
+    user: MAIL_USER,
+    pass: MAIL_PASS,
+  }
+});
 
 app.post("/feedbacks", async (request, response) => {
   const { type, comment, screenshot } = request.body;
@@ -13,7 +28,20 @@ app.post("/feedbacks", async (request, response) => {
       comment,
       screenshot
     }
-  })
+  });
+
+  await transport.sendMail({
+    from: "Equipe Feedget <oi@feedget.com>",
+    to: "Diego Fernandes <diego.schell.f@gmail.com>",
+    subject: "Novo feedback",
+    html: [
+      '<div style="font-family: sans-serif; font-size: 1rem; color: #111;">',
+      `<p>Tipo do feedback: ${type}</p>`,
+      `<p>Comentário: ${comment}</p>`,
+      '</div>'
+    ].join('\n')
+  });
+
   return response.status(201).json({ data: feedback });
 })
 
